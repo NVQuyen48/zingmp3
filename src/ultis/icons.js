0be3d59306559088367a40395513de8394bf6c5d
I'm Quyền <nguyenquyen4820@gmai.com>
2025-03-06 00:46:52 +0700

    v9

--- src/ultis/icons.js
+++ src/ultis/icons.js
@@ -1,5 +1,4 @@
 import { MdOutlineLibraryMusic, MdOutlineFeed  } from "react-icons/md";
-import { HiChartPie } from "react-icons/hi";
 import { TbChartArcs } from "react-icons/tb";
 import { AiOutlinePieChart } from "react-icons/ai";
 import { FaArrowLeft, FaArrowRight  } from "react-icons/fa";
